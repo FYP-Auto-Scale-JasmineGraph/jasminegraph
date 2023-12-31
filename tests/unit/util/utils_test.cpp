bf3b23e09f2820bc55913ad48fbede5eec2c105f
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2023-12-31 23:45:08 +0530

    Implement util functions for read and manipulate yaml definition files

--- tests/unit/util/utils_test.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "gtest/gtest.h"
-
-#include "../../../src/util/Utils.h"
-
-TEST(UtilsTest, TestGetJasmineGraphProperty) {
-    ASSERT_EQ(Utils::getJasmineGraphProperty("org.jasminegraph.server.host"), "localhost");
-}
