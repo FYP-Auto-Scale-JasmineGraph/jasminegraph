477249cd7be57e458c2a4f0f2cc2091a5e0b2cd8
thevindu-w <thevinduwijesekera99@gmail.com>
2024-02-28 02:41:51 +0530

    Add scripts to build minimal docker image

---
+++ minimal/Dockerfile.fs
@@ -0,0 +1,15 @@
+FROM busybox:stable-glibc
+
+COPY --chmod=755 files.tmp/lib/* /lib/x86_64-linux-gnu/
+COPY --chmod=755 files.tmp/bin/* /bin/
+
+RUN mkdir -p /home/ubuntu/software/jasminegraph/conf
+RUN mkdir -p /home/ubuntu/software/jasminegraph/k8s
+RUN mkdir -p /home/ubuntu/software/jasminegraph/metadb
+RUN mkdir -p /home/ubuntu/software/jasminegraph/performancedb
+
+COPY --chmod=755 files.tmp/JasmineGraph /home/ubuntu/software/jasminegraph/
+COPY --chmod=755 minimal/run-docker.sh /home/ubuntu/software/jasminegraph/
+COPY --chmod=644 conf/jasminegraph-server.properties /home/ubuntu/software/jasminegraph/conf/
+COPY --chmod=644 conf/hosts.txt /home/ubuntu/software/jasminegraph/conf/
+COPY --chmod=644 k8s/*.yaml /home/ubuntu/software/jasminegraph/k8s/
